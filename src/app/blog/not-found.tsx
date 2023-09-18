1229c54ae56d0a305e194f731fdd808b3b955d1d
Anshul Kanwar <me@anshulkanwar.com>
2023-09-18 14:38:01 +0530

    fix missing icon

--- src/app/blog/not-found.tsx
+++ src/app/blog/not-found.tsx
@@ -1,10 +1,11 @@
-import { Frown } from "lucide-react";
+import Image from "next/image";
+import frown from "@/components/icons/frown.svg"
 
 export default function NotFound() {
   return (
     <div className="mt-40">
       <div className="flex gap-2 justify-center items-center">
-        <Frown size={40}/>
+        <Image src={frown} alt="not found"/>
         <h2 className='text-4xl font-bold'>404</h2>
       </div>
       <p className="text-center mt-5">blog not found</p>
