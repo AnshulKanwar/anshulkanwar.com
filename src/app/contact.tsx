cc5f075d43bd42ebe3588b749fd24fedaec741ac
Anshul Kanwar <me@anshulkanwar.com>
2023-09-18 14:25:34 +0530

    update icons

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -1,22 +1,32 @@
 import Message from "@/components/message";
-import { Github, Linkedin, Mail } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+
+import github from "@/app/icons/github.svg"
+import mail from "@/app/icons/mail.svg"
+import x from "@/app/icons/x.svg"
+import linkedin from "@/app/icons/linkedin.svg"
 
 const contacts = [
   {
     name: "Github",
     href: "https://github.com/AnshulKanwar",
-    icon: <Github size={18} />,
+    icon: <Image src={github} width={18} alt="Github.com" />
   },
   {
     name: "Mail",
     href: "mailto:work@anshulkanwar.com",
-    icon: <Mail size={18} />,
+    icon: <Image src={mail} width={18} alt="mail" />
+  },
+  {
+    name: "X",
+    href: "https://x.com/_0xanshul",
+    icon: <Image src={x} width={18} alt="X.com"/>
   },
   {
     name: "Linkedin",
     href: "https://www.linkedin.com/in/anshul-kanwar/",
-    icon: <Linkedin size={18} />,
+    icon: <Image src={linkedin} width={18} alt="linkedin.com"/>
   },
 ];
 
@@ -25,7 +35,7 @@ export default function Contact() {
     <div>
       <h2 className="mb-6 text-xl font-bold">Contact</h2>
       <div className="grid grid-cols-1 sm:grid-cols-[1fr_2fr] gap-10">
-        <div className="flex flex-row justify-between sm:flex-col gap-3">
+        <div className="flex flex-row justify-between sm:flex-col gap-2">
           {contacts.map(({ name, href, icon }) => (
             <div key={name}>
               <Link
